28022834cf39b959dd5ad9fdeb54f9c341e03cee
Corey <coreyforbes864@gmail.com>
2019-12-09 10:29:00 -0500

    first commit for deliverable2

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your message?");
+            Console.ReadLine();
+
+            String s = "corey";
+            StringBuilder sb = new StringBuilder();
+            for (char c; ; s.ToCharArray())
+            {
+            
+                sb.Append((char)('c' - 'a' + 1));
+            }
+        }
+    }
+}
